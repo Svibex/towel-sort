3e7f77f21559099aa9c1be70629fc65893219b68
Svibex <77160861+Svibex@users.noreply.github.com>
2021-03-10 21:50:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function towelSort (matrix) {
-  return [].concat(matrix);
+    let tmp = '',
+        newArray = [],
+    for(let i=0; i < matrix.length; i++){
+        for(let j=0; j<matrix[i].length; j++) {
+            tmp = tmp + matrix[i][j];
+        }
+        newArray.push(tmp);
+        tmp = ''
+    }
+  return newArray;
 }
